e8f1f31146c25d49b2a66e678d8269e34512ac7c
maanyahub <166611747+maanyahub@users.noreply.github.com>
2024-04-13 22:31:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = {
         "today i miss my waanuuuu so let me pen down ",
         " missing that podaaaa , cuteeee face and voice , vc toooo sm , isokkayy ilyysm",
         "mumbai ko bless kar rahi hai , lagta hai jaise shrey waanya ko miss kar rahaa , koi nhi 2 days to go , podaaaa idiot",
-        "okieee aise hi likh raha tha just to tell u that i miss u darling , kisses le lo and thode se de bhi dooooo, and enjoyy karoooo", 
+        "okieee aise hi likh raha tha just to tell u that i miss u darling , kisses le lo and thode se de bhi dooooo, and enjoyy karoooo, bahut saara pyaar", 
        
     ],
     date:"2024-04-13T20:15:00",
